128d1c6cf31c478c3849205e3f0454124b4596ee
Gopi Guntupalli <gguntupalli@enova.com>
2016-08-18 13:31:01 -0500

    play audio sounds

--- app/src/main/java/com/bignerdranch/android/beatbox/Sound.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/Sound.java
@@ -1,11 +1,15 @@
 package com.bignerdranch.android.beatbox;
 
+import android.content.Intent;
+
 /**
  * Created by gguntupalli on 8/16/16.
  */
 public class Sound {
     private String mAssetPath;
     private String mName;
+
+    private Integer mSoundId;
 
     public Sound(String assetPath) {
         mAssetPath = assetPath;
@@ -20,5 +24,13 @@ public class Sound {
 
     public String getName() {
         return mName;
+    }
+
+    public Integer getSoundId() {
+        return mSoundId;
+    }
+
+    public void setSoundId(Integer soundId) {
+        mSoundId = soundId;
     }
 }
